f6b9716b85813ae9a7fdf5f00944ab481846925b
Andres Martin <andres.martin9309@gmail.com>
2026-03-16 18:56:44 -0600

    fix: build deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,30 @@
-import adapter from '@sveltejs/adapter-static'; // Asegúrate de tener el adapter-static
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
+import adapter from "@sveltejs/adapter-static"; // Asegúrate de tener el adapter-static
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Le decimos a Svelte que procese archivos .svelte y .md
-  extensions: ['.svelte', '.md'],
-  
+  extensions: [".svelte", ".md"],
+
   preprocess: [
     vitePreprocess(),
     mdsvex({
-      extensions: ['.md']
-    })
+      extensions: [".md"],
+    }),
   ],
 
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: "404.html", // Importante para que no falle en refrescos
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
 
 export default config;
